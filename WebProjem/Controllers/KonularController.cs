80daca33cfbc67e672b19b05ea4ccaa506aa4593
Aysen1 <aysenacun@gmail.com>
2018-12-29 11:16:10 +0200

    ilk

---
+++ WebProjem/Controllers/KonularController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProjem.Controllers
+{
+    public class KonularController : Controller
+    {
+        public ViewResult Dersler()
+        {
+            return View();
+        }
+        public ViewResult Bolumler()
+        {
+            return View();
+        }
+    }
+}
