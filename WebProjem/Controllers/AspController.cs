61b522b087950dd8bf5ec20175a169dacebc7b69
Aysen1 <aysenacun@gmail.com>
2018-12-29 12:09:58 +0200

    denemeler

---
+++ WebProjem/Controllers/AspController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProjem.Controllers
+{
+    public class AspController : Controller
+    {
+        public ViewResult Anasayfa()
+        {
+            return View();
+        }
+        public ViewResult Hakkimda()
+        {
+            return View();
+        }
+        public ViewResult Konular()
+        {
+            return View();
+        }
+        public ViewResult Haberler()
+        {
+            return View();
+        }
+        public ViewResult iletisim()
+        {
+            return View();
+        }
+    }
+}
